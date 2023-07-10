ab283522fa7421261230ea36728db72d99fdefe3
subhra-56 <97516074+subhra-56@users.noreply.github.com>
2023-07-10 13:53:26 +0530

    Exporting to csv

---
+++ scrap2.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas
+
+username=[]
+rating=[]
+reviews=[]
+detailed_reviews=[]
+url="https://www.commonsensemedia.org/app-reviews/khan-academy-kids/user-reviews/adult"
+result=requests.get(url).text
+doc=BeautifulSoup(result,"html.parser")
+
+user_reviews=doc.find_all(class_="user-generated-content")
+for user_review in user_reviews:
+    name=user_review.find_all(class_="user-summary__name")
+    username.append(name[0].a.string)
+    rate=user_review.find_all(class_="rating__score")
+    ratings=rate[0].find_all(class_="active")
+    rating.append(len(ratings)-1)
+    rvw=user_review.find_all(class_="user-generated-content__title")
+    if(rvw==[]):
+        reviews.append("N.A.")
+    else:
+        reviews.append(rvw[0].string)
+        rvw=user_review.find_all(class_="user-generated-content__title")
+    drvw=user_review.find_all(class_="reveal__content collapse")
+    if(drvw==[]):
+        detailed_reviews.append("N.A.")
+    else:
+        detailed_reviews.append(drvw[0].string.strip())
+
+dictionary={'User':username,'Rating out of 5':rating,'Review Title':reviews,'Review':detailed_reviews}
+
+df=pandas.DataFrame(dictionary)
+to_csv=df.to_csv("D:\PS\khanacademyadultrev.csv")
