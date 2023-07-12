8086ef6218c3371a0f7a9bf8ef8be93d24930e37
subhra-56 <97516074+subhra-56@users.noreply.github.com>
2023-07-13 04:23:24 +0530

    Update steam_scrap3.py

--- steam_scrap3.py
+++ steam_scrap3.py
@@ -1,7 +1,12 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-import pandas
+import pandas as pd
+
+dated=[]
+recmnd=[]
+hours=[]
+commnt=[]
 
 i=1
 
@@ -25,12 +30,22 @@ while i<10:
         print(recmd.find(class_="hours").string.split('h')[0])
         print(comment.get_text(strip=True).split('Early Access Review')[1])
         print()
+
+        dated.append(date.string[8:])
+        recmnd.append(recmd.find(class_="title").string)
+        hours.append(recmd.find(class_="hours").string.split('h')[0])
+        commnt.append(comment.get_text(strip=True).split('Early Access Review')[1])
     
     try:
         link=doc.find("input", {"name": "userreviewscursor"})["value"]
         url=f"https://steamcommunity.com/app/438100/homecontent/?userreviewscursor={link}&userreviewsoffset={(i-1)*10}&p={i}&workshopitemspage={i}&readytouseitemspage={i}&mtxitemspage={i}&itemspage={i}&screenshotspage={i}&videospage={i}&artpage={i}&allguidepage={i}&webguidepage={i}&integratedguidepage={i}&discussionspage={i}&numperpage=10&browsefilter=toprated&browsefilter=toprated&l=english&appHubSubSection=10&filterLanguage=default&searchText=&maxInappropriateScore=50&forceanon=1"
     except:
         break
+    
+dictionary={'Date':dated,'Recommendation':recmnd,'Hours Active':hours,'Comment':commnt}
+
+df=pd.DataFrame(dictionary)
+to_csv=df.to_csv("D:\PS\steamreview1.csv")
       
 
     
